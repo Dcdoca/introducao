24393a3a1c3d9b7af88c5d10a0167df02bec65ae
Dcdoca <antoniomarcorrs@gmail.com>
2024-06-14 21:31:34 -0300

    1commit

---
+++ src/main/java/com/poo/introducao/IntroducaoApplication.java
@@ -0,0 +1,15 @@
+package com.poo.introducao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Scanner;
+
+@SpringBootApplication
+public class IntroducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroducaoApplication.class, args);
+		System.out.println("ola mundo");
+	}
+
+}
