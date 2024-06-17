2db15673464e3b454bc82ff87b2552e63f7b5657
Dcdoca <antoniomarcorrs@gmail.com>
2024-06-17 12:03:41 -0300

    resolucao do exercicio 1

--- src/main/java/com/poo/introducao/Lista01.java
+++ src/main/java/com/poo/introducao/Lista01.java
@@ -1,8 +1,23 @@
 package com.poo.introducao;
+import java.util.Scanner;
 
 public class Lista01 {
-    void ex1() {
-        System.out.println("hello word");
+    public void ex1() {
+        int num1;
+        int num2;
+        int resultado;
+
+        System.out.println("Digite um número: ");
+        Scanner sc = new Scanner(System.in);
+
+        num1 = sc.nextInt();
+        System.out.println("Digite mais um número: ");
+
+        num2 = sc.nextInt();
+
+        resultado = num1 + num2;
+
+        System.out.println("A soma de " + num1 + " + " + num2 + " é igual à: " + resultado);
+        sc.close();
     }
-    
 }
