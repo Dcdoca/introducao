92f481ddc52bfa1a2d0d74a564160d51e3ab86ff
Dcdoca <antoniomarcorrs@gmail.com>
2024-06-19 10:32:55 -0300

    resolucao do exercicio 02

--- src/main/java/com/poo/introducao/Lista02.java
+++ src/main/java/com/poo/introducao/Lista02.java
@@ -18,5 +18,32 @@ public class Lista02 {
 
         scanner.close();
     }
+    
+    public void ex02(){
+        Scanner scanner = new Scanner(System.in);
+        int numero01;
+        int numero02;
+
+        System.out.println("digite o primeiro numero");
+         numero01 = scanner.nextInt();
+
+        System.out.println("digite o segundo numero");
+         numero02 = scanner.nextInt();
+
+        if(numero01 > numero02){
+            System.out.println("O número(" + numero01 + ") é maior que o número (" + numero02 +").");
+        }
+
+        else if (numero01 < numero02){
+            System.out.println("O segundo número(" + numero02 + ") é maior que o número (" + numero01 +").");
+
+        }
+        else{
+            System.out.println("Ambos numeros são iguais.");
+        }
+        scanner.close();
+
+}
+
     }
 
