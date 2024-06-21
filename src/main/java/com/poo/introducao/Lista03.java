48494cc7d1dc89a00086e69bad4040c4d19b0a11
Dcdoca <antoniomarcorrs@gmail.com>
2024-06-21 11:28:10 -0300

    resolucao do exercicio 05

--- src/main/java/com/poo/introducao/Lista03.java
+++ src/main/java/com/poo/introducao/Lista03.java
@@ -101,6 +101,52 @@ public class Lista03 {
             }
             while (advinhador != numeroAdvinhar);
         }
+    public void ex05(){
+        String login = "Antonio01";
+        String senha = "1234Ab";
+        String loginDigitado;
+        String senhaDigitada;
+        int tentativas = 3;
+        boolean respostaErrada = true;
+
+        do {
+            System.out.println("==== Login ====");
+            System.out.print("Login: ");
+            Scanner sc = new Scanner(System.in);
+            loginDigitado = sc.nextLine();
+
+            System.out.print("Senha: ");
+            senhaDigitada = sc.nextLine();
+            //Verifica a senha
+            if ( (login.equals(loginDigitado)) && (senha.equals(senhaDigitada))) {
+                System.out.println("Bem vindo!");
+                respostaErrada = false;
+            } else {
+                tentativas--;
+                // Qual foi o erro?
+                // Verificação dos 2
+                //     login != loginDigitado
+                if ( !(login.equals(loginDigitado)) && !(senha.equals(senhaDigitada))) {
+                    System.out.println("Login e senha incorretos!");
+                // Verificação da senha
+                } else if ( !senha.equals(senhaDigitada) ) {
+                    System.out.println("Senha incorreta!");
+                // Verificação do login
+                } else {
+                    System.out.println("Login incorreto!");
+                }
+
+                if (tentativas == 0){
+                    System.out.println("Senha bloqueada!");
+                } else {
+                    System.out.println(tentativas + " tentativas restantes");
+                }
+            }
+        } while (tentativas > 0 && respostaErrada);
+    
+    }
+
+
     }
     
                
