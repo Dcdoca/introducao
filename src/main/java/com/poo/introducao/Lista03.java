7c8add12be8ba2c42e4cb8d6470d36a4f60ec1c3
Dcdoca <antoniomarcorrs@gmail.com>
2024-06-21 14:34:28 -0300

    resolucao do exercicio 7

--- src/main/java/com/poo/introducao/Lista03.java
+++ src/main/java/com/poo/introducao/Lista03.java
@@ -166,7 +166,29 @@ public class Lista03 {
         }
             scanner.close();
     }
+    public void ex07(){
 
+        /* Aproveitando o exercício anterior, escreva um programa que consiga
+        construir um retângulo. O usuário deve informar a largura e a altura em
+        asteriscos */
+
+        // Cria um objeto Scanner para receber a entrada do usuário
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a largura do retângulo: ");
+        int largura = scanner.nextInt();
+
+        System.out.print("Digite a altura do retângulo: ");
+        int altura = scanner.nextInt();
+
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < largura; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        scanner.close();
+     }
     }
     
                
